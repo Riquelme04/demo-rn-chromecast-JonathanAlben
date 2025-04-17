91295cbf61affd0af0ff8d95f487681921c9b9c0
Riquelme04 <6176721@collegemv.qc.ca>
2025-04-17 12:38:02 -0400

    Mise à jour final de App.tsx
    Implémentation visuelle dans l'app mobile de bouton qui on été fabriqué individuellement dans chacune de leur composante.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Text, View, ImageBackground, StyleSheet } from "react-native";
+import { SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import { CastButton } from "react-native-google-cast";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
 import { PlayButton } from "./components/PlayButton";
 import { initVideo } from "./util/initVideo.jsx";
+import { RewindButton } from "./components/RewindButton";
+import { ForwardButton } from "./components/ForwardButton";
+import { VolumeBar } from "./components/VolumeBar";
+
 
 export default function App() {
   const {
     handlePlayPause,
     startVideo,
     stopVideo,
+    handleRewind,
+    handleForward,
+    handleVolumeChange,
     isPlay,
     isStarted,
     showPlay,
@@ -18,21 +26,56 @@ export default function App() {
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={s.container}>
-        <View style={s.workspace}>
-          <Text>Bienvenu au DBill Cast avec React Native! v1.3</Text>
-          <Text>Status : {status}</Text>
-          <CastButton style={s.castButton} />
-          <PlayButton
-            showPlay={showPlay}
-            isPlay={isPlay}
-            isStarted={isStarted}
-            startVideo={startVideo}
-            stopVideo={stopVideo}
-            handlePlayPause={handlePlayPause}
-          />
-        </View>
-      </SafeAreaView>
+      <ImageBackground
+          source={{ uri: "https://random-image-pepebigotes.vercel.app/api/random-image" }}
+          style={{ flex: 1, width: '100%', height: '100%'  }}
+          resizeMode="cover"
+         >
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'transparent' }}>
+          <View style={s.workspace}>
+            <Text style={styles.text}> Manette d'Alben et Jonathan 🎮 </Text>
+            <Text style={styles.text}>État : {status}</Text>
+            <CastButton style={s.castButton} />
+            {showPlay && (
+              <View>
+                <RewindButton onPress={handleRewind} />
+                <PlayButton
+                  isPlay={isPlay}
+                  isStarted={isStarted}
+                  showPlay={showPlay}
+                  startVideo={startVideo}
+                  stopVideo={stopVideo}
+                  handlePlayPause={handlePlayPause}
+                />
+                <ForwardButton onPress={handleForward} />
+              </View>
+              )}
+          </View>
+            
+          {showPlay && (
+            <View style={styles.volumeContainer}>
+              <VolumeBar onVolumeChange={handleVolumeChange} />
+            </View>
+          )}
+          </SafeAreaView>
+        </ImageBackground>
+        
+      
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  
+  text: {
+    color: "#fff",
+    fontSize: 30,
+    marginTop: 10,
+    alignContent: "center"
+  },
+  
+  volumeContainer: {
+    alignItems: "center",
+    marginBottom: 40,
+  },
+});
